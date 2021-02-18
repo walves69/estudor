73c4fe4455da5d73e10b00b9964536a4d5c1e1ec
Wllyssys Alves <ulima69@gmail.com>
2021-02-18 00:35:40 -0300

    Incorporando Capítulo 2 e 3 do curso na pasta DSA.

---
+++ DSA/Arquivos-Cap03/Exercicios-Cap03.R
@@ -0,0 +1,56 @@
+# Lista de Exercícios - Capítulo 3
+
+# Obs: Caso tenha problemas com a acentuação, consulte este link:
+# https://support.rstudio.com/hc/en-us/articles/200532197-Character-Encoding
+
+# Configurando o diretório de trabalho
+# Coloque entre aspas o diretório de trabalho que você está usando no seu computador
+# Não use diretórios com espaço no nome
+setwd("C:/FCD/BigDataRAzure/Cap02")
+getwd()
+
+# Exercício 1 - Pesquise pela função que permite listar todos os arquivo no diretório de trabalho
+
+# Exercício 2 - Crie um dataframe a partir de 3 vetores: um de caracteres, um lógico e um de números
+
+# Exercício 3 - Considere o vetor abaixo. 
+# Crie um loop que verifique se há números maiores que 10 e imprima o número e uma mensagem no console.
+
+# Criando um Vetor
+vec1 <- c(12, 3, 4, 19, 34)
+vec1
+
+# Exercício 4 - Conisdere a lista abaixo. Crie um loop que imprima no console cada elemento da lista
+lst2 <- list(2, 3, 5, 7, 11, 13)
+lst2
+
+# Exercício 5 - Considere as duas matrizes abaixo. 
+# Faça uma multiplicação element-wise e multiplicação normal entre as materizes
+mat1 <- matrix(c(1:50), nrow = 5, ncol = 5, byrow = T)
+mat1
+mat2 <- t(mat1)
+mat2
+
+# Multiplicação element-wise
+
+# Multiplicação de matrizes
+
+# Exercício 6 - Crie um vetor, matriz, lista e dataframe e faça a nomeação de cada um dos objetos
+
+# Exercício 7 - Considere a matriz abaixo. Atribua valores NA de forma aletória para 50 elementos da matriz
+# Dica: use a função sample()
+mat2 <- matrix(1:90, 10)
+mat2
+
+# Exercício 8 - Para a matriz abaixo, calcule a soma por linha e por coluna
+mat1 <- matrix(c(1:50), nrow = 5, ncol = 5, byrow = T)
+mat1
+
+# Exercício 9 - Para o vetor abaixo, ordene os valores em ordem crescente
+a <- c(100, 10, 10000, 1000)
+a
+
+# # Exercício 10 - Imprima no console todos os elementos da matriz abaixo que forem maiores que 15
+mat1 <- matrix(c(1:50), nrow = 5, ncol = 5, byrow = T)
+mat1
+
